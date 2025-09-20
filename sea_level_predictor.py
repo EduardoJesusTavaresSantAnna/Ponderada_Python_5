5f06d77ee251f82bad1d84667624326fc3872e47
Eduardo Jesus Tavares Sant'Anna <eduardo.santanna@sou.inteli.edu.br>
2025-09-19 23:26:55 -0300

    fix: Código implementado corretamente
    Updated variable names to Portuguese for consistency.

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,30 +4,24 @@ from scipy.stats import linregress
 import numpy as np
 
 def draw_plot():
-    # Read data from file
     df = pd.read_csv('epa-sea-level.csv')
 
-    # Create scatter plot
     plt.figure(figsize=(10, 6))
     plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
 
-    # Create first line of best fit (all data)
-    res_all = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
-    years_all = np.arange(df['Year'].min(), 2051)
-    plt.plot(years_all, res_all.intercept + res_all.slope * years_all, 'r', label='Best fit all years')
+    resultado_todos = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    anos_todos = np.arange(df['Year'].min(), 2051)
+    plt.plot(anos_todos, resultado_todos.intercept + resultado_todos.slope * anos_todos, 'r', label='Ajuste todos os anos')
 
-    # Create second line of best fit (year >= 2000)
-    df_recent = df[df['Year'] >= 2000]
-    res_recent = linregress(df_recent['Year'], df_recent['CSIRO Adjusted Sea Level'])
-    years_recent = np.arange(2000, 2051)
-    plt.plot(years_recent, res_recent.intercept + res_recent.slope * years_recent, 'g', label='Best fit 2000+')
+    df_recente = df[df['Year'] >= 2000]
+    resultado_recente = linregress(df_recente['Year'], df_recente['CSIRO Adjusted Sea Level'])
+    anos_recente = np.arange(2000, 2051)
+    plt.plot(anos_recente, resultado_recente.intercept + resultado_recente.slope * anos_recente, 'g', label='Ajuste 2000+')
 
-    # Add labels and title
     plt.xlabel('Year')
     plt.ylabel('Sea Level (inches)')
     plt.title('Rise in Sea Level')
     plt.xticks(np.arange(1850, 2080, 25))
 
-    # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
